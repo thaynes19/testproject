d1ff1f374abe91235396f8a309769cb12a4f7015
Cole Horner <colehorner94@gmail.com>
2018-09-26 13:42:49 -0500

    Attaching intent to buttons
    Attaching the intent to open the respective sites to the buttons.

--- App/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/Contact_Us.java
+++ App/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/Contact_Us.java
@@ -1,7 +1,10 @@
 package t.angelicafabila.thecornellianapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Contact_Us extends AppCompatActivity {
 
@@ -11,6 +14,22 @@ public class Contact_Us extends AppCompatActivity {
         setContentView(R.layout.activity_contact_us);
     }
 
-
-
+    /* Activity to send user to the Cornellian website. */
+    public void website(View view){
+        Intent websiteIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://blogs.cornellcollege.edu/cornellian/"));
+        startActivity(websiteIntent);
+    }
+    /* Activity to send user to the Cornellian Facebook page. */
+    public void facebook(View view){
+        Intent facebookIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://www.facebook.com/TheCornellian/"));
+        startActivity(facebookIntent);
+    }
+    /* Activity to send user to the Cornellian Twitter page. */
+    public void twitter(View view){
+        Intent twitterIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/")); //TODO
+        startActivity(twitterIntent);
+    }
 }
