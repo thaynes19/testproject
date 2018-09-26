43ec512e8a3e3f4771225048cda66c20bc9b5c7c
JNorris96 <jnorris19@cornellcollege.edu>
2018-09-26 01:24:22 -0500

    End Tuesday Workday
    Added a primitive form for the "contact us page". I have placeholder images for the social media and website buttons. Added some drawables and did some clean up on some things we didnt need

---
+++ App/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/Contact_Us.java
@@ -0,0 +1,16 @@
+package t.angelicafabila.thecornellianapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contact_Us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+
+
+
+}
