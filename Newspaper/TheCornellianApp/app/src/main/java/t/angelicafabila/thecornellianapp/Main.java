37e364157677abcd7093143efbc8126d34956217
JNorris96 <jnorris19@cornellcollege.edu>
2018-09-30 21:43:14 -0500

    Fixing Main Class

--- Newspaper/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/Main.java
+++ Newspaper/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/Main.java
@@ -2,6 +2,7 @@ package t.angelicafabila.thecornellianapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.util.Log;
@@ -12,7 +13,6 @@ import android.widget.Button;
 //import android.widget.Spinner;
 //import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
-<<<<<<< HEAD
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import org.jsoup.Jsoup;
@@ -20,12 +20,6 @@ import org.jsoup.nodes.Document;
 
 public class Main extends AppCompatActivity
 {
-
-=======
-
-public class Main extends AppCompatActivity
-{
->>>>>>> a4097c49b3973a6247740684a033abacdce60c0c
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -43,18 +37,10 @@ public class Main extends AppCompatActivity
 //                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.items));
 //        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //        menuSpinner.setAdapter(myAdapter);
-
-<<<<<<< HEAD
-
-        configureMainButton();
-        configureAboutUsButton();
-        configureContactUsButton();
-=======
         configureMainButton();
         configureAboutUsButton();
         configureContactUsButton();
         configureArchiveButton();
->>>>>>> a4097c49b3973a6247740684a033abacdce60c0c
     }
 
     private void configureMainButton()
